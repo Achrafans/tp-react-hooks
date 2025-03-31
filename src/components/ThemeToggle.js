d9a709babd6be9efc8046a7d6e9af04810804aac
achrafansari <achraf.ansari@centoria.fr>
2025-03-31 21:24:28 +0000

    add hook useLocalStorage

--- src/components/ThemeToggle.js
+++ src/components/ThemeToggle.js
@@ -1,12 +1,21 @@
 import React, { useContext } from 'react';
 import { ThemeContext } from '../App';
+import useLocalStorage from '../hooks/useLocalStorage';
 
 const ThemeToggle = () => {
   const { isDarkTheme, setIsDarkTheme } = useContext(ThemeContext);
+  const [storedTheme, setStoredTheme] = useLocalStorage('theme', isDarkTheme);
+
+
+  const toggleTheme = () => {
+    setIsDarkTheme(!isDarkTheme);
+    setStoredTheme(!isDarkTheme);
+  }
+
   
   return (
     <button
-      onClick={() => setIsDarkTheme(!isDarkTheme)}
+      onClick={toggleTheme}
       className={`px-5 py-2 rounded ${
         isDarkTheme 
           ? 'bg-dark text-light border border-light' 
---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+
+const useLocalStorage = (key, initialValue) => {
+    const [storedValue, setStoredValue] = useState(() => {
+        try {
+            const item = localStorage.getItem(key);
+            return item ? JSON.parse(item) : initialValue;
+        } catch (error) {
+            console.error("Erreur lors de la récupération depuis localStorage", error);
+            return initialValue;
+        }
+    });
+
+    useEffect(() => {
+        try {
+            localStorage.setItem(key, JSON.stringify(storedValue));
+        } catch (error) {
+            console.error("Erreur lors de l'enregistrement dans localStorage", error);
+        }
+    }, [key, storedValue]);
+
+    return [storedValue, setStoredValue];
+};
+
+export default useLocalStorage;
